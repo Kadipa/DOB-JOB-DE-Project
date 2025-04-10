c3ec6367707493789afb6517279e8c1cd9bce538
Kadipa <kadiparaungmyohan@gmail.com>
2025-04-10 23:32:25 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,3 +24,27 @@ variable "redshift_db_name" {
   description = "Redshift database name"
   default     = "dobdb"
 }
+
+variable "glue_database_name" {
+  description = "The name of the Glue catalog database."
+  type        = string
+  default     = "dob-db-glue-redshift"
+}
+
+variable "glue_crawler_name" {
+  description = "The name of the Glue crawler."
+  type        = string
+  default     = "dob_jobs_crawler"
+}
+
+variable "s3_bucket_path" {
+  description = "The S3 path where raw data is stored."
+  type        = string
+  default     = "s3://de-project-s3-datalake-bucket-south-east-region/dob_jobs/nyc_dob_job_data"
+}
+
+variable "iam_role" {
+  description = "IAM role ARN used by Glue and Redshift Spectrum."
+  type        = string
+  default     = "arn:aws:iam::354294457076:role/RedshiftSpectrumRole"
+}
