a30b8105b0925a8e5568f58dd5d2ba577bd94d4c
Kadipa <kadiparaungmyohan@gmail.com>
2025-04-10 23:31:57 +0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,3 +46,27 @@ resource "aws_iam_role_policy_attachment" "redshift_s3_policy_attachment" {
   role       = aws_iam_role.redshift_s3_access_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
+
+
+resource "aws_glue_catalog_database" "dob_database" {
+  name = var.glue_database_name
+}
+
+
+resource "aws_glue_crawler" "dob_crawler" {
+  name          = var.glue_crawler_name
+  database_name = aws_glue_catalog_database.dob_database.name
+  role          = var.iam_role
+
+  s3_target {
+    path = var.s3_bucket_path
+  }
+
+  configuration = jsonencode({
+    Version: 1.0,
+    Grouping: {
+      TableGroupingPolicy: "CombineCompatibleSchemas"
+    }
+  })
+  
+}
